0363a877e103739846b7d66eef4c8ba1fd240aa5
florence <loracodes3@gmail.com>
2024-10-01 23:59:22 +0300

    a function to write a happy brthday song

---
+++ index.js
@@ -0,0 +1,8 @@
+// 
+function happyBirthday(){
+    console.log('Happy Birthday to you!');
+    console.log('Happy Birthday to you!');
+    console.log('Happy Birthday dear you');
+    console.log('Happy Birthday to you!');
+
+}
