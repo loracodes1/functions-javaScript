087f01b537c7c838511ce53f2980b8dfab9ab8a1
florence <loracodes3@gmail.com>
2024-10-02 00:20:23 +0300

    calling a function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// 
+// A FUNCTION--a section pof reusable code
+//--Declare code once, use it whenever you want
+//Call the function to execute the code
+
 function happyBirthday(){
     console.log('Happy Birthday to you!');
     console.log('Happy Birthday to you!');
     console.log('Happy Birthday dear you');
     console.log('Happy Birthday to you!');
 }
-happyBirthday //to execute the code within the function, we have to call the function by its name
+happyBirthday()//to execute the code within the function, we have to call the function by its name
+happyBirthday() //execute it the second time and as many times as you want
