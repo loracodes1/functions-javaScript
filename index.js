8d87810cb772eec858039e126aa46a4c3397c8e2
florence <loracodes3@gmail.com>
2024-10-02 00:03:33 +0300

    calling a function by its name so as to execute the code

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function happyBirthday(){
     console.log('Happy Birthday to you!');
     console.log('Happy Birthday dear you');
     console.log('Happy Birthday to you!');
-
 }
+happyBirthday //to execute the code within the function, we have to call the function by its name
