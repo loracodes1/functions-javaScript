3c143606f420bc044d7d2d2ae6f63b17861f1292
florence <loracodes3@gmail.com>
2024-10-02 08:41:23 +0300

    checking whether a number is even or not

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ function multiply(x,y){
     return x*y;
 }
 console.log(multiply(2,3))
+
+function subtraction(x,y){
+    return y-x;
+}
+console.log(subtraction(2,3))
+
+function isEven(number){
+    if (number %2 ===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+console.log(isEven(10))
+console.log(isEven(11))
