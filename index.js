4af80de018ccfa282f4bf89b9646ddfd2ff7ddf3
florence <loracodes3@gmail.com>
2024-10-02 08:48:19 +0300

    ternary operator

--- index.js
+++ index.js
@@ -42,5 +42,11 @@ function isEven(number){
     }
 
 }
-console.log(isEven(10))
-console.log(isEven(11))
+console.log(isEven(10)) //true
+console.log(isEven(11)) //false
+
+//TERNARY OPERATOR
+function isEvenNumber(num){
+    return num %2 ===0 ? true: false
+}
+console.log(isEvenNumber(21)) //will return false
