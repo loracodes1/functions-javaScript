fbd0826d8d14c43b1e3f5456f24a1483edaf86b8
florence <loracodes3@gmail.com>
2024-10-02 07:34:01 +0300

    literal template

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 //--Declare code once, use it whenever you want
 //Call the function to execute the code
 
-function happyBirthday(){
+function happyBirthday(userName , age){
     console.log('Happy Birthday to you!');
     console.log('Happy Birthday to you!');
-    console.log('Happy Birthday dear you');
-    console.log('Happy Birthday to you!');
+    console.log(`Happy Birthday dear ${userName}`);
+    console.log(`Your age is ${age} years old!`);
 }
 happyBirthday()//to execute the code within the function, we have to call the function by its name
 happyBirthday() //execute it the second time and as many times as you want
+happyBirthday("Loracodes", 37) //
