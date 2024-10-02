348e2071ab9582e02f2969d099457e21ca1605e7
florence <loracodes3@gmail.com>
2024-10-02 07:48:45 +0300

    pass different arguments

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 function happyBirthday(userName , age){
     console.log('Happy Birthday to you!');
     console.log('Happy Birthday to you!');
-    console.log(`Happy Birthday dear ${userName}`);
+    console.log(`Happy Birthday dear ${userName}!`);
     console.log(`Your age is ${age} years old!`);
 }
 happyBirthday()//to execute the code within the function, we have to call the function by its name
 happyBirthday() //execute it the second time and as many times as you want
 happyBirthday("Loracodes", 37) 
+happyBirthday("Spongebob",20)
 //the order in which you write the parameter matters happyBirthday(userName,age) isnt the same as happyBirthday(age,userName)
